eaa7544615531c9710f0c7f663c380a5942ca654
Hyunjin Kwon <kwonhyunjeen@gmail.com>
2025-06-28 19:16:44 +0900

    chore: initialize project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-gray-50 to-blue-50">
+      Let's go customizing your shoes!
+    </div>
+  );
+}
+
+export default App;
