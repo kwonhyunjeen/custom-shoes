cca3618fffb62f5cba796b62becc02d8809d3ca1
Hyunjin Kwon <kwonhyunjeen@gmail.com>
2025-07-09 01:46:37 +0900

    feat: feat: add camera controls to 3d shoe viewer

--- src/components/3d/Scene3D.tsx
+++ src/components/3d/Scene3D.tsx
@@ -1,5 +1,4 @@
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
 import { Shoes } from "./Shoes";
 
 export const Scene3D: React.FC = () => {
@@ -8,8 +7,6 @@ export const Scene3D: React.FC = () => {
       <Canvas camera={{ position: [5, 2, 5], fov: 50 }}>
         <axesHelper args={[5]} />
         <gridHelper />
-        <OrbitControls />
-        <directionalLight position={[3, 3, 3]} />
         <Shoes />
       </Canvas>
     </div>
--- src/components/3d/Shoes.tsx
+++ src/components/3d/Shoes.tsx
@@ -1,13 +1,20 @@
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
-import { useLoader } from "@react-three/fiber";
-import { useThree } from "@react-three/fiber";
+import { useLoader, useThree } from "@react-three/fiber";
+import type { ThreeEvent } from "@react-three/fiber";
+import {} from "@react-three/fiber";
 import * as THREE from "three";
+import { CameraControls } from "@react-three/drei";
+import { useRef } from "react";
 
 export const Shoes: React.FC = () => {
   const { raycaster } = useThree();
+  const cameraControlsRef = useRef<CameraControls>(null!);
+
   const gltf = useLoader(GLTFLoader, "/models/custom.glb");
 
-  const shoesClick = () => {
+  const shoesClick = (event: ThreeEvent<MouseEvent>) => {
+    event.stopPropagation();
+
     const intersects = raycaster.intersectObjects(gltf.scene.children, true);
     if (intersects.length > 0) {
       const mesh = intersects[0].object as THREE.Mesh;
@@ -20,8 +27,16 @@ export const Shoes: React.FC = () => {
   };
 
   return (
-    <mesh>
+    <>
+      <CameraControls
+        ref={cameraControlsRef}
+        enabled={true}
+        dollyToCursor={true}
+        minDistance={0.5}
+        maxDistance={10}
+      />
+      <directionalLight position={[3, 3, 3]} />
       <primitive object={gltf.scene} onClick={shoesClick} />
-    </mesh>
+    </>
   );
 };
