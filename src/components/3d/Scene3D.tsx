7b9e3893f52511283d7966c10acabf3204536ce0
Hyunjin Kwon <kwonhyunjeen@gmail.com>
2025-07-12 18:18:18 +0900

    feat: add improved lighting for realistic glass appearance

--- src/components/3d/Scene3D.tsx
+++ src/components/3d/Scene3D.tsx
@@ -4,9 +4,7 @@ import { Shoes } from "./Shoes";
 export const Scene3D: React.FC = () => {
   return (
     <div className="h-full bg-gradient-to-br from-gray-50 to-gray-100">
-      <Canvas camera={{ position: [5, 2, 5], fov: 50 }}>
-        <axesHelper args={[5]} />
-        <gridHelper />
+      <Canvas camera={{ position: [5, 2, 5], fov: 50 }} shadows>
         <Shoes />
       </Canvas>
     </div>
